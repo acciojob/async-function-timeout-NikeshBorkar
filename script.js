d3aefb6c19f0cebea47ef77cafb46e9f4660da76
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-10-25 16:51:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click",async()=>{
+	const text=document.getElementById("text").value
+	const delay=document.getElementById("delay").value
+
+	if(text&&delay){
+		function promise(){
+			return new Promise((resolve)=>{
+				setTimeout(()=>{
+					resolve(text)
+				},delay)
+			})
+		}
+
+		const data= await promise()
+		if(data){
+			document.getElementById("output").innerText=data
+		}
+		
+	}
+	
+})
